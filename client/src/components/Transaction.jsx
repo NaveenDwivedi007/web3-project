cd5a5ee147b59c726d6b0ab491f091915e5902fc
Naveen Dwivedi <naveen.kumar2@pristyncare.com>
2022-01-03 19:33:24 +0530

    flie structure done

---
+++ client/src/components/Transaction.jsx
@@ -0,0 +1,10 @@
+const Transaction = ()=>{
+    return (
+        <div>
+            <h1>
+                Transaction
+            </h1>
+        </div>
+    )
+}
+export default Transaction
---
+++ client/src/components/index.js
@@ -0,0 +1,9 @@
+export { default as Loader } from './layout/Loader'
+export { default as Navbar } from './layout/Navbar'
+export { default as Footer } from './layout/Footer'
+export { default as Services } from './Services'
+export { default as Welcome } from './Welcome'
+export { default as Transaction } from './Transaction'
+
+
+
